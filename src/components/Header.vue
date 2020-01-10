c071dfca3d11731a1457b5a2ecd352fc2f60b1bb
digitalfront-dev <dev.digitalfront@gmail.com>
2020-01-10 11:59:39 -0300

    master

---
+++ src/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <nav>
+    <ul class="uk-navbar-nav">
+      <router-link exact-active-class="uk-active" tag="li" to="/">
+        <a>Home</a>
+      </router-link>
+      <router-link exact-active-class="uk-active" tag="li" to="/sobre">
+        <a>Sobre</a>
+      </router-link>
+    </ul>
+  </nav>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import UIkit from 'uikit'
+import Icons from 'uikit/dist/js/uikit-icons'
 import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import router from './services/router'
+import store from './services/store'
+import '@/assets/sass/main.sass'
+
+UIkit.use(Icons)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  UIkit,
   store,
   render: h => h(App)
 }).$mount('#app')
